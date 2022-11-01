f6bc41dacf859825262f2e11a3a1665e419d0550
Oscar Bernal <odbg2013@gmail.com>
2022-10-31 20:04:13 -0500

    add user

--- src/App.js
+++ src/App.js
@@ -1,13 +1,32 @@
-function App() {
+import { useState } from 'react'
+import AddUserForm from './forms/AddUserForm'
+import UserTable from './tables/UserTable'
+
+const App = () => {
+  const usersData = [
+    { id: 1, name: 'Tania', username: 'floppy' },
+    { id: 2, name: 'Craig', username: 'silicon' },
+    { id: 3, name: 'Ben', username: 'sphere' },
+  ]
+
+  const [users, setUsers] = useState(usersData)
+
+  const addUser = (user) => {
+    users.id = user.length + 1
+    setUsers([...users, user])
+  }
+
   return (
     <div className='container'>
       <h1>CRUD App with hooks</h1>
       <div className='flex-row'>
         <div className='flex-large'>
           <h2>Add user</h2>
+          <AddUserForm addUser={addUser} />
         </div>
         <div className='flex-large'>
           <h2>View users</h2>
+          <UserTable users={users} />
         </div>
       </div>
     </div>
---
+++ src/tables/UserTable.jsx
@@ -0,0 +1,31 @@
+export default function UserTable({ users }) {
+  return (
+    <table>
+      <thead>
+        <tr>
+          <th>Name</th>
+          <th>Username</th>
+          <th>Actions</th>
+        </tr>
+      </thead>
+      <tbody>
+        {users.length > 0 ? (
+          users.map((user) => (
+            <tr key={user.id}>
+              <td>{user.name}</td>
+              <td>{user.username}</td>
+              <td>
+                <button className='button muted-button'>Edit</button>
+                <button className='button muted-button'>Delete</button>
+              </td>
+            </tr>
+          ))
+        ) : (
+          <tr>
+            <td colSpan={3}>No users</td>
+          </tr>
+        )}
+      </tbody>
+    </table>
+  )
+}
